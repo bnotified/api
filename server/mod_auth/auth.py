7068acda29631d58469f9e6e543699325d69a9fc
Giancarlo Anemone <ganemone@gmail.com>
2015-03-14 17:09:05 -0400

    Fix docstring linting errors

--- server/mod_auth/auth.py
+++ server/mod_auth/auth.py
@@ -1,3 +1,4 @@
+"""This module contains functions for handling user authentication."""
 import flask
 import json
 from flask_login import login_user
@@ -7,7 +8,7 @@ from server.login_manager import login_manager
 
 @login_manager.user_loader
 def load_user(user_id: int) -> User:
-    """Returns a user from the database based on their id
+    """Return a user from the database based on their id.
 
     :param user_id: a users unique id
     :return: User object with corresponding id, or none if user does not exist
@@ -16,14 +17,13 @@ def load_user(user_id: int) -> User:
 
 
 def handle_basic_auth(request: flask.Request) -> User:
-    """Verifies a request using BASIC auth
+    """Verify a request using BASIC auth.
 
     :param request: flask request object
-    :return: User object corresponding to login information, or none if user does not exist
+    :return: User object corresponding to login information, or none if
+    user does not exist
     """
     auth = request.authorization
-    log.log(logging.INFO, request.headers)
-    log.log(logging.INFO, auth)
     if not auth:
         return None
     return User.query.filter_by(
@@ -33,18 +33,17 @@ def handle_basic_auth(request: flask.Request) -> User:
 
 
 def login(request: flask.Request) -> flask.Response:
-    """Handle a login request from a user
+    """Handle a login request from a user.
 
     :param request: incoming request object
     :return: flask response object
     """
-
     user = handle_basic_auth(request)
-    log.log(logging.INFO, user)
 
     if user:
         login_user(user, remember=True)
-        return flask.Response(json.dumps({}), status=200, mimetype='application/json')
+        body = json.dumps({})
+        return flask.Response(body, status=200, mimetype='application/json')
     return flask.Response(
         'Could not verify your access level for that URL.\n'
         'You have to login with proper credentials', 401,

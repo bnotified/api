8ef6d6102c5834d579ebb224873ee20ba43cc38c
Giancarlo Anemone <ganemone@gmail.com>
2015-03-25 13:55:43 -0400

    Add util logging function

--- server/logger.py
+++ server/logger.py
@@ -3,3 +3,7 @@ import logging
 logger = logging.getLogger('werkzeug')
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
+
+
+def log(message):
+    logger.log(logging.INFO, message)
